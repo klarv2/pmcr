85ca7ab4303cbc69a34e58f1a7ecf093a11f54f8
klarv2 <propertymanagecr@gmail.com>
2019-11-22 10:53:29 -0800

    Updates

---
+++ precache-manifest.1695ea27d559ee6377949ae4c631f82d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11520ffeb0005c7c7ac119cb3ea59774",
+    "url": "/propertymanagecr/index.html"
+  },
+  {
+    "revision": "7a76efc6b4d5f8368880",
+    "url": "/propertymanagecr/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8924f944cbe34ced6226",
+    "url": "/propertymanagecr/static/css/main.04455136.chunk.css"
+  },
+  {
+    "revision": "7a76efc6b4d5f8368880",
+    "url": "/propertymanagecr/static/js/2.a768ef90.chunk.js"
+  },
+  {
+    "revision": "8924f944cbe34ced6226",
+    "url": "/propertymanagecr/static/js/main.040da442.chunk.js"
+  },
+  {
+    "revision": "ad4f2b9de5d476abbe06",
+    "url": "/propertymanagecr/static/js/runtime~main.737d5533.js"
+  }
+]);
